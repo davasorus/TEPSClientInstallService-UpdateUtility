0f4f14a514ec4247d0e200df7197b50d2cb24f79
Sean Davitt <davitts2@gmail.com>
2022-10-09 11:06:46 -0400

    code maid clean up

--- Classes/agentUpdateClass.cs
+++ Classes/agentUpdateClass.cs
@@ -195,11 +195,10 @@ namespace TEPSClientInstallService_UpdateUtility.Classes
 
             string A = returnAssemblyInformation(Path.Combine(serviceInstallPath, serviceName));
 
-            if(A == null)
+            if (A == null)
             {
                 A = "-1";
             }
-
 
             //this removes the separators in the version number
             string B = A.Replace(".", "");
